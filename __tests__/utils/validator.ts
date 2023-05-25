e6d83e946d7d49d9a0f3011071aef2496dc9e741
Osaremhen Ukpebor <osas.solo@outlook.com>
2023-05-25 14:17:24 +0100

    Updated validator.ts test

--- __tests__/utils/validator.ts
+++ __tests__/utils/validator.ts
@@ -1,38 +1,38 @@
 import {isNameValid, isBankVerificationNumberValid, isAccountNumberValid, isPinValid, isAmountValid} from "../../utils/validator";
 
 describe("validator.ts tests", () => {
-    test("isNameValid test", () => {
+    it("should expect a name containing only letters and between 0 and at most 100 characters", () => {
         expect(isNameValid("Osaremhen")).toBe(true);
     });
-    test("isNameValid test", () => {
+    it("should expect a name containing only letters and between 0 and at most 100 characters", () => {
         expect(isNameValid("O12S")).toBe(false);
     });
 
-    test("isBankVerificationNumberValid test", () => {
+    it("should expect only 11 digits", () => {
         expect(isBankVerificationNumberValid("12345678901")).toBe(true);
     });
-    test("isBankVerificationNumberValid test", () => {
+    it("should expect only 11 digits", () => {
         expect(isBankVerificationNumberValid("1234567890")).toBe(false);
     });
 
-    test("isAccountNumberValid test", () => {
+    it("should expect only 10 digits", () => {
         expect(isAccountNumberValid("1234567890")).toBe(true);
     });
-    test("isAccountNumberValid test", () => {
+    it("should expect only 10 digits", () => {
         expect(isAccountNumberValid("12345678901")).toBe(false);
     });
 
-    test("isPinValid test", () => {
+    it("should expect only 4 digits", () => {
         expect(isPinValid("1234")).toBe(true);
     });
-    test("isPinValid test", () => {
+    it("should expect only 4 digits", () => {
         expect(isPinValid("123e")).toBe(false);
     });
 
-    test("isAmountValid test", () => {
+    it("should start with a positive number, have only 2 decimal places and be <= 5,000,000", () => {
         expect(isAmountValid("50000")).toBe(true);
     });
-    test("isAmountValid test", () => {
+    it("should start with a positive number, have only 2 decimal places and be <= 5,000,000", () => {
         expect(isAmountValid("6000000")).toBe(false);
     });
 });
